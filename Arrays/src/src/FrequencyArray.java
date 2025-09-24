e10e68c3c868f1c1f15bb0d158bc4f755d13816b
Harishkumar S <160011156+Harish6251@users.noreply.github.com>
2025-09-24 11:45:49 +0530

    Update FrequencyArray.java

--- Arrays/src/src/FrequencyArray.java
+++ Arrays/src/src/FrequencyArray.java
@@ -1,25 +1,40 @@
-package com.qsp.random;
 
-public class Frequency {
+package src;
 
-	public static void main(String[] args) {
-		
-		String s = "Harishhh";
-		
-		char [] ch = s.toCharArray();
-		boolean [] b = new boolean [ch.length];
-		
-		for(int i=0; i<ch.length;i++) {
-			int count = 1;
-			
-			for(int j=i+1;j<ch.length;j++) {
-				if(ch[i] == ch[j]) {
-					count++;
-					b[j] = true;
-				}
-			}
-			System.out.println(ch[i] + " occured "+ count + " times");
-		}
-	}
+import java.util.Scanner;
+
+public class RemoveDuplicate {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many elements do you want to enter?");
+        
+        int size = sc.nextInt();
+        
+        int[] a = new int[size];
+        boolean[] b = new boolean[size];
+        
+        System.out.println("Enter " + size + " numbers:");
+        for (int i = 0; i < a.length; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println("Duplicate elements are:");
+        for (int i = 0; i < a.length; i++) {
+            if (!b[i]) {
+                int count = 1;
+                for (int j = i + 1; j < a.length; j++) {
+                    if (a[i] == a[j]) {
+                        count++;
+                        b[j] = true;
+                    }
+                }
+                System.out.println(a[i] + " is occured" + count + " times");
+                }
+            }
+        }
+
+        sc.close();
+    }
 }
-
