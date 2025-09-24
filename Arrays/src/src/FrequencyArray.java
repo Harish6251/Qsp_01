0cbcc369d4a7a4f90eb3e2ca6c6beb2015f628e6
Harishkumar S <160011156+Harish6251@users.noreply.github.com>
2025-09-24 11:42:17 +0530

    Update FrequencyArray.java

--- Arrays/src/src/FrequencyArray.java
+++ Arrays/src/src/FrequencyArray.java
@@ -1,1 +1,25 @@
+package com.qsp.random;
+
+public class Frequency {
+
+	public static void main(String[] args) {
+		
+		String s = "Harishhh";
+		
+		char [] ch = s.toCharArray();
+		boolean [] b = new boolean [ch.length];
+		
+		for(int i=0; i<ch.length;i++) {
+			int count = 1;
+			
+			for(int j=i+1;j<ch.length;j++) {
+				if(ch[i] == ch[j]) {
+					count++;
+					b[j] = true;
+				}
+			}
+			System.out.println(ch[i] + " occured "+ count + " times");
+		}
+	}
+}
 
